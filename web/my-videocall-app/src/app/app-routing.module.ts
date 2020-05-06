20f471308a58a3454d0a7368dfef1d77694a90ea
jaroenchai tantipong <jaroechai@gmail.com>
2020-05-06 13:39:53 +0700

    init angular with material

---
+++ web/my-videocall-app/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },  
+  { path: 'home', component: HomeComponent, data: {animation: 'HomePage'} },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
